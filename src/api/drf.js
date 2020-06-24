7fee8a1ccc5b77eb199cbd5b26ea5b94b728a75b
yeonjudkwl <hyj9785@likelion.org>
2020-06-24 21:04:50 +0900

    200624 SERVER URL

---
+++ src/api/drf.js
@@ -0,0 +1,8 @@
+export default {
+    URL: 'http://127.0.0.1:8000/',
+    ROUTES: {
+        signup: 'rest-auth/registration/',
+        login: 'rest-auth/login/',
+        logout: 'rest-auth/logout/',
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// domain
+import SERVER from '@/api/drf'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+
+  },
+  getters: {
+
   },
   mutations: {
+
   },
   actions: {
+
   },
   modules: {
   }

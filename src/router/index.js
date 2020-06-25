9cbf85d2ad9800e5a901265c7b84e7d57e9470e2
yeonjudkwl <hyj9785@likelion.org>
2020-06-25 13:34:53 +0900

    200625 router.beforeEach

--- src/router/index.js
+++ src/router/index.js
@@ -36,4 +36,24 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  // Login 안해도 됨
+  const publicPages = ['Login', 'Signup' ,'Home', 'List']
+  // Login 되어있으면 안됨
+  const authPages = ['Login', 'Signup']
+  // 로그인 해야함
+  const authRequired = !publicPages.includes(to.name)
+  // 로그인 해서는 안 됨
+  const unauthRequired = authPages.includes(to.name)
+  const isLoggedIn = Vue.$cookies.isKey('auth-token')
+  
+  // 로그인 해야하는데 로그인 안 되어 있으면
+  authRequired && !isLoggedIn ? next({ name: 'Login'}) : next()
+  
+  // 로그인 해서는 안되는데 로그인 되어 있으면
+  if (unauthRequired && isLoggedIn) {
+      next({ name: 'Home'})
+  }
+})
+
 export default router

e88e887ee4113cc96ffc5165ff6b224ace33d9e1
Taein Kim <kti91@likelion.org>
2020-07-11 13:09:19 +0900

    200711 | updated FeedDetail

--- src/components/articles/FeedLogo.vue
+++ src/components/articles/FeedLogo.vue
@@ -1,60 +1,66 @@
 <template>
   <div class="logos">
     <div class="logos-left">
-      <i v-if="isLikeUser" @click="unlike(feed.id);isLike()" class="logo-img fas fa-heart heart-color"></i>
+      <i
+        v-if="isLikeUser"
+        @click="unlike(feed.id);isLike()"
+        class="logo-img fas fa-heart heart-color"
+      ></i>
       <i v-else @click="like(feed.id);isLike()" class="logo-img far fa-heart"></i>
-      <router-link :to="{ name: 'FeedDetail', params: { id: feed.id, feed: feed } }"><i class="logo-img far fa-comment"></i></router-link>
+      <router-link :to="{ name: 'FeedDetail', params: { id: feed.id } }">
+        <i class="logo-img far fa-comment"></i>
+      </router-link>
       <!-- <svg aria-label="Direct"  class="_8-yf5" fill="#262626" height="23" viewBox="-10 -13 48 48" width="23">
         <path d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"></path>
-      </svg> -->
+      </svg>-->
     </div>
     <div class="logos-right">
-        <i class="logo-img far fa-bookmark"></i>
+      <i class="logo-img far fa-bookmark"></i>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
-  name: 'FeedLogo',
+  name: "FeedLogo",
   props: {
-    feed: Object,
+    feed: Object
   },
-  data () {
+  data() {
     return {
-      isLikeUser: false,
-    }
+      isLikeUser: false
+    };
   },
   methods: {
-    ...mapActions(['like', 'unlike']),
-    isLikeUsers () {
+    ...mapActions(["like", "unlike"]),
+    isLikeUsers() {
       if (this.feed.like_users) {
-        this.feed.like_users.forEach( data => {
+        this.feed.like_users.forEach(data => {
           if (data.username === this.$store.state.username) {
-            this.isLikeUser = true
+            this.isLikeUser = true;
           }
-        })
+        });
       }
     },
-    isLike () {
-      this.isLikeUser = !this.isLikeUser
+    isLike() {
+      this.isLikeUser = !this.isLikeUser;
     }
   },
-  created () {
-    this.isLikeUsers()
-  },
-}
+  created() {
+    this.isLikeUsers();
+  }
+};
 </script>
 
 <style>
-.logos{
-  display:flex;
+.logos {
+  display: flex;
   justify-content: space-between;
   padding: 0 10px;
 }
-.logo-img{
+.logo-img {
   padding: 10px;
   transform: scale(1.5);
   cursor: pointer;
--- src/views/articles/FeedDetail.vue
+++ src/views/articles/FeedDetail.vue
@@ -2,26 +2,26 @@
   <div class="feed-detail-wrap">
     <div class="feed-detail">
       <div class="feed-detail-img">
-        <FeedImg :feed="feed"/>
+        <FeedImg :feed="detail" />
       </div>
       <div class="feed-detail-content">
-        <FeedTitle :feed="feed"/>
-        <p>{{ feed.content }}</p>
-        <FeedComment :feed="feed"/>
+        <FeedTitle :feed="detail" />
+        <p>{{ detail.content }}</p>
+        <FeedComment :feed="detail" />
         <div class="feed-detail-content-bottom">
-          <FeedLogo :feed="feed"/>
+          <FeedLogo :feed="detail" />
           <div class="content">
-            <p class="content-like">좋아요 {{ feed.like_count }}개</p>
-          </div> 
-          <FeedCommentForm :feed="feed"/>
+            <p class="content-like">좋아요 {{ detail.like_count }}개</p>
+          </div>
+          <FeedCommentForm :feed="detail" />
         </div>
       </div>
     </div>
-    <hr>
+    <hr />
     <div class="profile-feeds">
       <div v-for="f in detail.user.feed_set" :key="f.id" class="profile-feed">
         <div v-for="img in f.images" :key="img.id">
-          <img :src="'http://127.0.0.1:8000'+ img.image" alt="feed-image">
+          <img :src="'http://127.0.0.1:8000'+ img.image" alt="feed-image" />
         </div>
       </div>
     </div>
@@ -29,46 +29,47 @@
 </template>
 
 <script>
-import SERVER from '@/api/drf'
-import axios from 'axios'
-import FeedTitle from '@/components/articles/FeedTitle.vue'
-import FeedImg from '@/components/articles/FeedImg.vue'
-import FeedLogo from '@/components/articles/FeedLogo.vue'
-import FeedComment from '@/components/articles/FeedComment.vue'
-import FeedCommentForm from '@/components/articles/FeedCommentForm.vue'
+import SERVER from "@/api/drf";
+import axios from "axios";
+import FeedTitle from "@/components/articles/FeedTitle.vue";
+import FeedImg from "@/components/articles/FeedImg.vue";
+import FeedLogo from "@/components/articles/FeedLogo.vue";
+import FeedComment from "@/components/articles/FeedComment.vue";
+import FeedCommentForm from "@/components/articles/FeedCommentForm.vue";
 
 export default {
-  name: 'FeedDetail',
+  name: "FeedDetail",
   components: {
     FeedTitle,
     FeedImg,
     FeedLogo,
     FeedComment,
-    FeedCommentForm,
+    FeedCommentForm
   },
-  data () {
+  data() {
     return {
       detail: null
-    }
+    };
   },
   computed: {
-    feed () {
-      return this.$route.params.feed
-    } 
-  },
-  methods: {
-    fetchFeed (feedId) {
-      axios.get(SERVER.URL + `/articles/${feedId}/`)
-        .then ( res => {
-          this.detail = res.data
-        })
-        .catch(err => console.log(err.response.data))
+    feed() {
+      return this.$route.params.id;
     }
   },
-  created () {
-    this.fetchFeed(this.feed.id)
+  methods: {
+    fetchFeed(feedId) {
+      axios
+        .get(SERVER.URL + `/articles/${feedId}/`)
+        .then(res => {
+          this.detail = res.data;
+        })
+        .catch(err => console.log(err.response.data));
+    }
+  },
+  created() {
+    this.fetchFeed(this.feed);
   }
-}
+};
 </script>
 
 <style scoped>
@@ -82,7 +83,7 @@ export default {
   display: flex;
   margin-top: 30px;
   width: 50%;
-  border: 1px solid #DBDBDB;
+  border: 1px solid #dbdbdb;
   border-radius: 2px;
 }
 .feed-detail-img {
@@ -93,7 +94,7 @@ export default {
   position: relative;
 }
 .feed-detail-content p {
-   padding: 10px;
+  padding: 10px;
 }
 .content-like {
   color: rgb(126, 126, 126);
@@ -106,28 +107,28 @@ export default {
 hr {
   margin: 25px 0;
   width: 50%;
-  color: #DBDBDB;
+  color: #dbdbdb;
 }
-.profile-feeds{
+.profile-feeds {
   width: 70%;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   margin: 0 auto;
 }
-.profile-feed{
+.profile-feed {
   width: 200px;
   margin: 13px;
 }
-.profile-feed img{
+.profile-feed img {
   width: 100%;
 }
 @media screen and (max-width: 1000px) {
-  .profile-feed{
+  .profile-feed {
     flex: 25%;
     margin: 4%;
   }
-  .profile-feed img{
+  .profile-feed img {
     width: 100%;
   }
 }

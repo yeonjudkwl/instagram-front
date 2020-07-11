e9843074ce1124e6cf9a2f1dd0d70bc2117d3c69
yeonjudkwl <hyj9785@likelion.org>
2020-07-12 02:20:49 +0900

    200712 SearchTag

--- src/views/articles/SearchTag.vue
+++ src/views/articles/SearchTag.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    {{ $store.state.searched }}
+  <div class="search-feeds">
+    <div v-for="feed in $store.state.searched" :key="feed.id" class="search-feed">
+      <div v-for="img in feed.images" :key="img.id">
+        <img :src="'http://127.0.0.1:8000'+ img.image" alt="feed-image">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +16,27 @@ export default {
 </script>
 
 <style scoped>
-
+.search-feeds{
+  width: 70%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+}
+.search-feed{
+  width: 200px;
+  margin: 13px;
+}
+.search-feed img{
+  width: 100%;
+}
+@media screen and (max-width: 1000px) {
+  .search-feed{
+    flex: 25%;
+    margin: 4%;
+  }
+  .search-feed img{
+    width: 100%;
+  }
+}
 </style>

e38d586062e5e79513f41aaea0a94487fd5c7a7b
yeonjudkwl <hyj9785@likelion.org>
2020-07-11 23:56:20 +0900

    200711 search

--- src/views/articles/Search.vue
+++ src/views/articles/Search.vue
@@ -1,15 +1,44 @@
 <template>
-    <div>
-        {{ $store.state.searched }}
-    </div>
+  <ul>
+    <li v-for="result in $store.state.searched" :key="result.id">
+      <div class="li-wrap">
+        <div class="li-img"> 
+          <img :src="'http://127.0.0.1:8000'+result.profile_photo" alt="profile">
+        </div>
+        <div class="li-content">
+          <div>{{ result.username }}</div>
+          <div v-if="result.name" >{{ result.name }}</div>
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
-
+ name: 'Search.vue',
 }
 </script>
 
-<style>
-
+<style scoped>
+ul {
+  width: 40%;
+  margin: 0 auto;
+  border: 1px solid gray;
+  border-radius: 4px;
+  list-style-type: none;
+}
+.li-wrap {
+  display: flex;
+  justify-content: center;
+}
+.li-img {
+  margin-right: 50px;
+}
+.li-img img {
+  width: 80px;
+}
+.li-content {
+  width: 20%;
+}
 </style>
